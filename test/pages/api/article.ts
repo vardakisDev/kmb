d3f3abb5fed0135a425cb578021197c611a13423
Stelios Vardakis <s.vardakis@doubleip.com>
2023-03-11 17:52:59 +0200

    changes to API

--- test/pages/api/article.ts
+++ test/pages/api/article.ts
@@ -1,10 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Article from "@/model/article.model";
 import { PageWrapper } from "@/model/pageWrapper.model";
+import { articleRepo } from "@/repository/article.repository";
 import type { NextApiRequest, NextApiResponse } from "next";
-import data from "../../data.json";
-
-type SORT_TYPE = "NEW" | "OLD";
 
 export default function handler(
   req: NextApiRequest,
@@ -13,91 +11,15 @@ export default function handler(
   switch (req.method) {
     case "GET": {
       const searchKey = req.query.searchKey as string;
-      const sortType = req.query.sort as SORT_TYPE;
-      const groupedBySource = req.query.groupedBySource;
-      const { page = 0, limit = 10 } = req.query;
-      let articleData = data as Article[];
-      if (searchKey && searchKey.length > 0) {
-        articleData = handleSearchFilter(articleData, searchKey);
-      }
-      if (groupedBySource) {
-        // articleData = hanldeGroupBySource(articleData);
-      }
-      switch (sortType.toLocaleUpperCase()) {
-        case "NEW":
-          articleData = newFirst(articleData);
-          break;
-        case "OLD":
-          articleData = oldestFirst(articleData);
-          break;
-        default:
-          break;
-      }
-      const paginatedResult = handlePagination(
-        articleData,
+      const sortType = req.query.sort as string;
+      const { page = 0, limit = 6 } = req.query;
+      const filteredArticles = articleRepo.getArticleWithFilter(
         page as number,
-        limit as number
+        limit as number,
+        sortType,
+        searchKey
       );
-      res.status(200).send({
-        data: paginatedResult.data,
-        totalPages: paginatedResult.totalPages,
-        page: page as number,
-        limit: limit as number,
-      });
+      res.status(200).send(filteredArticles);
     }
   }
 }
-
-const oldestFirst = (articles: Article[]) =>
-  [...articles].sort(
-    (a, b) => Date.parse(a.publishedAt) - Date.parse(b.publishedAt)
-  );
-
-const newFirst = (articles: Article[]) =>
-  [...articles].sort(
-    (a, b) => Date.parse(b.publishedAt) - Date.parse(a.publishedAt)
-  );
-
-const handlePagination = (
-  articles: Article[],
-  page: number,
-  limit: number
-): { data: Article[]; totalPages: number } => {
-  // Calculate the start and end indices for the current page
-  const startIndex = (page - 1) * limit;
-  const endIndex = startIndex + limit;
-
-  // Get the articles for the current page
-  const articlesForPage = articles.slice(startIndex, endIndex);
-
-  // Calculate the total number of pages
-  const totalPages = Math.ceil(articles.length / limit);
-  return {
-    data: articlesForPage,
-    totalPages: totalPages,
-  };
-};
-
-const handleSearchFilter = (articles: Article[], title: string) => {
-  // Filter the articles by title
-  return articles.filter((article) =>
-    article.title.toLowerCase().includes(title.toLowerCase())
-  );
-};
-
-const hanldeGroupBySource = (articles: Article[]) => {
-  const groupedArticles: { [key: string]: Article[] } = {};
-  articles.forEach((article) => {
-    const sourceName = article.source.name;
-    if (!groupedArticles[sourceName]) {
-      groupedArticles[sourceName] = [];
-    }
-    groupedArticles[sourceName].push(article);
-  });
-  articles = Object.keys(groupedArticles).map((sourceName) => ({
-    source: { name: sourceName, id: groupedArticles[sourceName][0].source.id },
-    articles: groupedArticles[sourceName],
-  }));
-
-  return groupedArticles;
-};
---
+++ test/repository/article.repository.ts
@@ -0,0 +1,97 @@
+import Article from "@/model/article.model";
+import { PageWrapper } from "@/model/pageWrapper.model";
+import { flatMap, groupBy } from "lodash";
+import data from "../data.json";
+
+export const articleRepo = {
+  getAll: () => getAll(),
+  getArticleWithFilter: (
+    page: number,
+    limit: number,
+    sortType: string,
+    searchKey: string
+  ) => getArticleWithFilter(page, limit, sortType, searchKey),
+};
+
+const getArticleWithFilter = (
+  page: number,
+  limit: number,
+  sortType: string,
+  searchKey: string
+): PageWrapper<Article> => {
+  let articleData = data as Article[];
+  if (searchKey && searchKey.length > 0) {
+    articleData = handleSearchFilter(articleData, searchKey);
+  }
+  switch (sortType.toUpperCase()) {
+    case "NEW":
+      articleData = newFirst(articleData);
+      break;
+    case "OLD":
+      articleData = oldestFirst(articleData);
+      break;
+    case "GROUP":
+      const grouped = hanldeGroupBySource(articleData);
+      articleData = flatMap(grouped);
+      break;
+    default:
+      articleData = newFirst(articleData);
+      break;
+  }
+  const paginatedResult = handlePagination(
+    articleData,
+    page as number,
+    limit as number
+  );
+  return {
+    data: paginatedResult.data,
+    totalPages: paginatedResult.totalPages,
+    page: page as number,
+    limit: limit as number,
+  };
+};
+
+const getAll = () => {
+  return data;
+};
+
+const oldestFirst = (articles: Article[]) =>
+  [...articles].sort(
+    (a, b) => Date.parse(a.publishedAt) - Date.parse(b.publishedAt)
+  );
+
+const newFirst = (articles: Article[]) =>
+  [...articles].sort(
+    (a, b) => Date.parse(b.publishedAt) - Date.parse(a.publishedAt)
+  );
+
+const handlePagination = (
+  articles: Article[],
+  page: number,
+  limit: number
+): { data: Article[]; totalPages: number } => {
+  // Calculate the start and end indices for the current page
+  const startIndex = (page - 1) * limit;
+  const endIndex = startIndex + limit;
+
+  // Get the articles for the current page
+  const articlesForPage = articles.slice(startIndex, endIndex);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(articles.length / limit);
+  return {
+    data: articlesForPage,
+    totalPages: totalPages,
+  };
+};
+
+const handleSearchFilter = (articles: Article[], title: string) => {
+  // Filter the articles by title
+  return articles.filter((article) =>
+    article.title.toLowerCase().includes(title.toLowerCase())
+  );
+};
+
+const hanldeGroupBySource = (articles: Article[]) => {
+  return groupBy(articles, (a) => a.source.id);
+};
